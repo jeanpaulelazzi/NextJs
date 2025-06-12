59e7adf5f28d2ce3635bcd3babbb0775dfe18ca6
jeanpaulelazzi <jpazzi@outlook.com>
2025-06-12 15:25:09 +0300

    task One

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DoctorModule } from './doctor/doctor.module';
+import { PatientModule } from './patient/patient.module';
+import { DepartmentsModule } from './departments/departments.module';
 
 @Module({
-  imports: [],
+  imports: [DoctorModule, PatientModule, DepartmentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+async function bootstrap() {const app = await NestFactory.create(AppModule);
+  await app.listen
+  (process.env.PORT ?? 3000);
 }
 bootstrap();
+
