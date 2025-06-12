403d31e9ae5b9bc7d190cfe500bc100a6076bcc4
jeanpaulelazzi <jpazzi@outlook.com>
2025-06-12 22:31:16 +0300

    task two

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { DoctorModule } from './doctor/doctor.module';
 import { PatientModule } from './patient/patient.module';
 import { DepartmentsModule } from './departments/departments.module';
+import { PrismaClient } from '@prisma/client';
 
 @Module({
   imports: [DoctorModule, PatientModule, DepartmentsModule],
--- src/patient/patient.service.ts
+++ src/patient/patient.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
+
+
 @Injectable()
-export class PatientService {}
+export class PatientService {
+
+}
+
